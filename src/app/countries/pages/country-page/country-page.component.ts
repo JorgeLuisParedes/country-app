56fe2d15eb47fe2473bcc48a4d7abf088038c731
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-04 04:00:12 +0200

    Mostrar la información del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/contries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
 	selector: 'app-country-page',
@@ -9,15 +10,14 @@ import { switchMap } from 'rxjs';
 	styles: ``,
 })
 export class CountryPageComponent implements OnInit {
+	public country?: Country;
+
 	constructor(private activatedRoute: ActivatedRoute, private router: Router, private countriesService: CountriesService) {}
 
 	ngOnInit(): void {
 		this.activatedRoute.params.pipe(switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))).subscribe((country) => {
-			if (!country) {
-				return this.router.navigateByUrl('');
-			}
-			console.log('tenemos un pais');
-			return;
+			if (!country) return this.router.navigateByUrl('');
+			return this.country = country;
 		});
 	}
 }
