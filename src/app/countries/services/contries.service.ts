97dc89f039a2853093e705ce21db91e7d788ca12
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-04 19:57:12 +0200

    LocalStorage del Store

--- src/app/countries/services/contries.service.ts
+++ src/app/countries/services/contries.service.ts
@@ -14,7 +14,18 @@ export class CountriesService {
 		byRegion: { region: '', countries: [] },
 	};
 
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {
+		this.loadToLocalStorage();
+	}
+
+	private saveToLocalStorage() {
+		localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+	}
+
+	private loadToLocalStorage() {
+		if (!localStorage.getItem('cacheStore')) return;
+		this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+	}
 
 	private getCountriesRequest(url: string): Observable<Country[]> {
 		return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
@@ -30,16 +41,25 @@ export class CountriesService {
 
 	searchCapital(term: string): Observable<Country[]> {
 		const url = `${this.apiUrl}/capital/${term}`;
-		return this.getCountriesRequest(url).pipe(tap((countries) => (this.cacheStore.byCapital = { term, countries })));
+		return this.getCountriesRequest(url).pipe(
+			tap((countries) => (this.cacheStore.byCapital = { term, countries })),
+			tap(() => this.saveToLocalStorage())
+		);
 	}
 
 	searchCountry(term: string): Observable<Country[]> {
 		const url = `${this.apiUrl}/name/${term}`;
-		return this.getCountriesRequest(url).pipe(tap((countries) => (this.cacheStore.byCountries = { term, countries })));
+		return this.getCountriesRequest(url).pipe(
+			tap((countries) => (this.cacheStore.byCountries = { term, countries })),
+			tap(() => this.saveToLocalStorage())
+		);
 	}
 
 	searchRegion(region: Region): Observable<Country[]> {
 		const url = `${this.apiUrl}/region/${region}`;
-		return this.getCountriesRequest(url).pipe(tap((countries) => (this.cacheStore.byRegion = { region, countries })));
+		return this.getCountriesRequest(url).pipe(
+			tap((countries) => (this.cacheStore.byRegion = { region, countries })),
+			tap(() => this.saveToLocalStorage())
+		);
 	}
 }
