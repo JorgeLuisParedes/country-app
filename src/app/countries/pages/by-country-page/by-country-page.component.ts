5c672e43518237b05b42c52902d4879aff92c615
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-04 02:53:34 +0200

    Tarea Paìs - Región

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/contries.service';
 
 @Component({
-  selector: 'app-by-country-page',
-  templateUrl: './by-country-page.component.html',
-  styles: ``
+	selector: 'app-by-country-page',
+	templateUrl: './by-country-page.component.html',
+	styles: ``,
 })
 export class ByCountryPageComponent {
+	public countries: Country[] = [];
 
+	constructor(private countriesService: CountriesService) {}
+
+	searchByCountry(term: string) {
+		this.countriesService.searchCountry(term).subscribe((countries) => {
+			this.countries = countries;
+		});
+	}
 }
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/contries.service';
 
 @Component({
-  selector: 'app-by-region-page',
-  templateUrl: './by-region-page.component.html',
-  styles: ``
+	selector: 'app-by-region-page',
+	templateUrl: './by-region-page.component.html',
+	styles: ``,
 })
 export class ByRegionPageComponent {
+	public countries: Country[] = [];
 
+	constructor(private countriesService: CountriesService) {}
+
+	searchByRegion(region: string) {
+		this.countriesService.searchRegion(region).subscribe((countries) => {
+			this.countries = countries;
+		});
+	}
 }
