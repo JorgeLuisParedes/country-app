c597d6d885610a19f3d6a180c9245c0363eddcb4
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-04 13:38:22 +0200

    LoadingComponent - Mostrar el indicador de carga

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-	declarations: [AboutPageComponent, HomePageComponent, SidebarComponent, ContactPageComponent, SearchBoxComponent],
+	declarations: [AboutPageComponent, HomePageComponent, SidebarComponent, ContactPageComponent, SearchBoxComponent, LoadingSpinnerComponent],
 	imports: [CommonModule, RouterModule],
-	exports: [AboutPageComponent, HomePageComponent, SidebarComponent, SearchBoxComponent],
+	exports: [AboutPageComponent, HomePageComponent, SidebarComponent, SearchBoxComponent, LoadingSpinnerComponent],
 })
 export class SharedModule {}
