f32509da0308b472aa9e33f629d44d8da17adedb
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-04 02:12:57 +0200

    CountryTable Component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -6,9 +6,10 @@ import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.com
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 @NgModule({
-	declarations: [ByCapitalPageComponent, ByCountryPageComponent, ByRegionPageComponent, CountryPageComponent],
+	declarations: [ByCapitalPageComponent, ByCountryPageComponent, ByRegionPageComponent, CountryPageComponent, CountryTableComponent],
 	imports: [CommonModule, CountriesRoutingModule, SharedModule],
 })
 export class CountriesModule {}
